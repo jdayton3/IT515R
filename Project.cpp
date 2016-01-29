a12dc8cfa3de86703ca867967286acc8ad3c53bb
Jonathan <jonathan_dayton@bitbucket.org>
2016-01-29 10:01:58 -0700

    Complete Phase 2

--- Project.cpp
+++ Project.cpp
@@ -2,22 +2,33 @@
 #include <cmath>
 #include <algorithm>
 #include <vector>
-
-unsigned const int SIZE = 1024;
-
-unsigned const int HSIZE = SIZE;
-unsigned const int VSIZE = SIZE;
+#include <cstdint>
 
 int main() {
+	uint32_t iterations;// = 0;
+	float epsilon;// = 0.1;
+	//uint32_t SIZE = 1024;
+	uint32_t HSIZE;// = SIZE; //num columns
+	uint32_t VSIZE;// = SIZE; //num rows
 
-	std::vector<std::vector<float>> theArray(SIZE, std::vector<float>(SIZE));
-	std::vector<std::vector<float>> tempArray(SIZE, std::vector<float>(SIZE));
-	int countReps = 0;
+	//std::cout.write(reinterpret_cast<char const *>(&iterations), sizeof(uint32_t));
+	//std::cout.write(reinterpret_cast<char const *>(&epsilon), sizeof(float));
+	//std::cout.write(reinterpret_cast<char const *>(&VSIZE), sizeof(uint32_t));
+	//std::cout.write(reinterpret_cast<char const *>(&HSIZE), sizeof(uint32_t));
+
+	std::cin.read(reinterpret_cast<char *>(&iterations), sizeof(uint32_t));
+	std::cin.read(reinterpret_cast<char *>(&epsilon), sizeof(float));
+	std::cin.read(reinterpret_cast<char *>(&VSIZE), sizeof(uint32_t));
+	std::cin.read(reinterpret_cast<char *>(&HSIZE), sizeof(uint32_t));
+
+	std::vector<std::vector<float>> theArray(VSIZE, std::vector<float>(HSIZE));
+	std::vector<std::vector<float>> tempArray(VSIZE, std::vector<float>(HSIZE));
+
 	bool cont = true;
 
-	for (unsigned int i = 0; i < VSIZE; i++) {
-		for (unsigned int j = 0; j < HSIZE; j++) {
-			if (i == 0 || i == HSIZE - 1 ||
+	for (uint32_t i = 0; i < VSIZE; i++) {
+		for (uint32_t j = 0; j < HSIZE; j++) {
+			/*if (i == 0 || i == HSIZE - 1 ||
 				j == 0 || j == VSIZE - 1) {
 				theArray[i][j] = 0;
 				tempArray[i][j] = 0;
@@ -25,25 +36,26 @@ int main() {
 			else {
 				theArray[i][j] = 50;
 				tempArray[i][j] = 50;
-			}
+			}*/
+			std::cin.read(reinterpret_cast<char *>(&theArray[i][j]), sizeof(float));
+			//std::cout.write(reinterpret_cast<char const *>(&theArray[i][j]), sizeof(float));
 		}
 	}
 
 	while (cont) {
-		++countReps;
-		for (unsigned int i = 1; i < VSIZE - 1; i++) {
-			for (unsigned int j = 1; j < HSIZE - 1; j++) {
+		++iterations;
+		for (uint32_t i = 1; i < VSIZE - 1; i++) {
+			for (uint32_t j = 1; j < HSIZE - 1; j++) {
 				tempArray.at(i).at(j) = (theArray.at(i - 1).at(j) + theArray.at(i + 1).at(j) + theArray.at(i).at(j - 1) + theArray.at(i).at(j + 1)) / 4.0f;
 			}
 		}
 		std::swap(tempArray, theArray);
 		bool stable = true;
-		for (unsigned int i = 1; i < VSIZE - 1; i++) {
-			for (unsigned int j = 1; j < HSIZE - 1; j++) {
+		for (uint32_t i = 1; i < VSIZE - 1; i++) {
+			for (uint32_t j = 1; j < HSIZE - 1; j++) {
 				bool s;
-				float threshold = 0.1;
 				float avg = (theArray.at(i - 1).at(j) + theArray.at(i + 1).at(j) + theArray.at(i).at(j - 1) + theArray.at(i).at(j + 1)) / 4.0f;
-				if (std::abs(theArray.at(i).at(j) - avg) < threshold) {
+				if (std::abs(theArray.at(i).at(j) - avg) < epsilon) {
 					s = true;
 				}
 				else {
@@ -56,9 +68,17 @@ int main() {
 		}
 		if (stable) {
 			cont = false;
-			std::cout << "\nIt ran " << countReps << " times.\n\n";
+			//std::cout << iterations;
+			//std::cout.write(reinterpret_cast<char const *>(&iterations), sizeof(uint32_t));
 		}
 	}
 
+	for (uint32_t i = 0; i < VSIZE; i++) {
+		for (uint32_t j = 0; j < HSIZE; j++) {
+			std::cout.write(reinterpret_cast<char const *>(&theArray[i][j]), sizeof(float));
+		}
+	}
+
+//	system("pause");
 	return 0;
 }
